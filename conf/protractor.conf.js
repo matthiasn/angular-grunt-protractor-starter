d40dd093d5878476d27e25758d2a244ab61bb837
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-08-22 11:14:47 +0200

    PhantomJS added

--- conf/protractor.conf.js
+++ conf/protractor.conf.js
@@ -2,6 +2,8 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../test/protractor/spec.js'], 
   multiCapabilities: [{
+    'browserName': 'phantomjs'
+  }, {
     'browserName': 'firefox'
   }, {
     'browserName': 'chrome'
